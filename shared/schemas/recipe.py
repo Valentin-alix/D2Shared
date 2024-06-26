96277ac20e098bd96412ec18244c2df8cf30c0f3
Valentin Alix <valentin-alix.850@outlook.fr>
2024-06-26 11:16:10 +0200

    update

--- shared/schemas/recipe.py
+++ shared/schemas/recipe.py
@@ -1,4 +1,5 @@
 from EzreD2Shared.shared.schemas.base import BaseSchemaOrm
+from EzreD2Shared.shared.schemas.ingredient import IngredientSchema
 from EzreD2Shared.shared.schemas.item import ItemSchema
 from EzreD2Shared.shared.schemas.job import JobSchema
 
@@ -7,7 +8,7 @@ class RecipeSchema(BaseSchemaOrm):
     id: int
     result_item_id: int
     result_item: ItemSchema
-    # ingredients: list[IngredientSchema]
+    ingredients: list[IngredientSchema]
     job_id: int
     job: JobSchema
 
