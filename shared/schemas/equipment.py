61af69fc853b2bbf73b5a522bda7fd51cda18c8f
Valentin Alix <valentin-alix.850@outlook.fr>
2024-07-13 16:43:21 +0200

    remove count attempt

--- shared/schemas/equipment.py
+++ shared/schemas/equipment.py
@@ -10,7 +10,6 @@ class ReadEquipmentSchema(BaseEquipmentSchema):
     id: int
     lines: list[LineSchema]
     exo_line: LineSchema | None
-    count_attempt: int
 
     def __eq__(self, value: object) -> bool:
         return isinstance(value, ReadEquipmentSchema) and self.id == value.id
